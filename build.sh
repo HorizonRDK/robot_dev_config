01a8ca869511de7fcd400148de6cd577d8201600
kao.zhu <kao.zhu@horizon.ai>
2023-01-16 13:50:23 +0800

    update build scripts

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cat <<EOF
 Usage: bash -e $0 <options>
 available options:
 -p|--platform: set platform ([X3|J5|X86])
+-s|--selction: add colcon build --packages-select [PKG_NAME]
 -h|--help
 EOF
 exit
@@ -20,9 +21,10 @@ if [ $# -lt 1 ];then
    show_usage
 fi
 
+PACKAGE_SELECTION=""
 
 PLATFORM_OPTS=(X3 J5 X86)
-GETOPT_ARGS=`getopt -o p:b:a:l:c:f:u:h -al platform:,help -- "$@"`
+GETOPT_ARGS=`getopt -o p:s:h -al platform:,selction:,help -- "$@"`
 eval set -- "$GETOPT_ARGS"
 
 while [ -n "$1" ]
@@ -35,6 +37,12 @@ do
         echo "invalid platform: $platform"
         show_usage
       fi
+      ;;
+    -s|--selction)
+      selction=$2
+      shift 2
+      echo "colcon build selction: $selction"
+      PACKAGE_SELECTION="--packages-select $selction"
       ;;
     -h|--help) show_usage; break;;
     --) break ;;
@@ -55,8 +63,10 @@ if [ $platform == "X86" ]; then
   echo "build X86"
   ln -s `pwd`/../sysroot_docker/usr_x86 `pwd`/../sysroot_docker/usr
 
+  echo "PACKAGE_SELECTION: $PACKAGE_SELECTION"
+  
   ## 开始编译
-  colcon build --packages-select dnn_node --cmake-args -DPLATFORM_X86=ON -DTHIRD_PARTY=`pwd`/../sysroot_docker
+  colcon build $PACKAGE_SELECTION --cmake-args -DPLATFORM_X86=ON -DTHIRD_PARTY=`pwd`/../sysroot_docker
   
 else
   ## 配置交叉编译工具链
@@ -69,10 +79,14 @@ else
   elif [ $platform == "J5" ]; then
     echo "build J5"
     ln -s `pwd`/../sysroot_docker/usr_j5 `pwd`/../sysroot_docker/usr
+    # 只编译J5平台的package
+    ./robot_dev_config/j5_build.sh
   fi
   
+  echo "PACKAGE_SELECTION: $PACKAGE_SELECTION"
+  
   ## 开始交叉编译
-  colcon build \
+  colcon build $PACKAGE_SELECTION \
     --merge-install \
     --cmake-force-configure \
     --cmake-args \
