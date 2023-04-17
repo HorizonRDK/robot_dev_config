d502824445b825f9a890b5eda46883ea418a2565
kao.zhu <kao.zhu@horizon.ai>
2023-04-17 14:19:29 +0800

    Change hobot dnn repo branch to feature-j5

--- j5_build.sh
+++ j5_build.sh
@@ -5,15 +5,11 @@ touch \
     ./src/ament/googletest/googletest/COLCON_IGNORE \
     ./src/app/line_follower \
     ./src/app/xr_robot \
-    ./src/box/ros-perception/COLCON_IGNORE \
     ./src/box/ros-planning/COLCON_IGNORE \
     ./src/box/hobot_audio/COLCON_IGNORE \
     ./src/box/hobot_interactions/COLCON_IGNORE \
     ./src/box/hobot_perception/COLCON_IGNORE \
-    ./src/box/hobot_dnn/dnn_node_example/COLCON_IGNORE \
-    ./src/box/hobot_dnn/dnn_node_sample/COLCON_IGNORE \
     ./src/box/hobot_hdmi/COLCON_IGNORE \
-    ./src/box/hobot_cv/COLCON_IGNORE \
     ./src/box/hobot_slam/COLCON_IGNORE \
     ./src/box/hobot_sensors/COLCON_IGNORE \
     ./src/box/ros-navigation/COLCON_IGNORE \
@@ -23,16 +19,13 @@ touch \
     ./src/box/ros-tracing/ros2_tracing/tracetools_read/COLCON_IGNORE \
     ./src/box/ros-tracing/ros2_tracing/tracetools_test/COLCON_IGNORE \
     ./src/box/ros-tracing/ros2_tracing/tracetools_trace/COLCON_IGNORE \
-    ./src/box/ros-perception/vision_opencv/COLCON_IGNORE \
     ./src/box/ros-navigation/navigation2/nav2_system_tests/COLCON_IGNORE \
     ./src/box/ros-navigation/navigation2/nav2_rviz_plugins/COLCON_IGNORE \
     ./src/box/ros-navigation/navigation2/smac_planner/COLCON_IGNORE \
     ./src/eclipse-cyclonedds/cyclonedds/COLCON_IGNORE \
-    ./src/osrf/COLCON_IGNORE \
     ./src/ros/kdl_parser/COLCON_IGNORE \
     ./src/ros/resource_retriever/COLCON_IGNORE \
     ./src/ros/robot_state_publisher/COLCON_IGNORE \
-    ./src/ros/ros_environment/COLCON_IGNORE \
     ./src/ros/ros_tutorials/COLCON_IGNORE \
     ./src/ros/urdfdom/COLCON_IGNORE \
     ./src/ros/urdfdom_headers/COLCON_IGNORE \
@@ -44,38 +37,18 @@ touch \
     ./src/tros/examples/rclcpp/executors/COLCON_IGNORE \
     ./src/tros/examples/rclcpp/services/COLCON_IGNORE \
     ./src/tros/examples/rclcpp/timers/COLCON_IGNORE \
-    ./src/tros/geometry2/COLCON_IGNORE \
-    ./src/tros/launch/COLCON_IGNORE \
-    ./src/tros/launch_ros/COLCON_IGNORE \
-    ./src/tros/message_filters/COLCON_IGNORE \
-    ./src/tros/mimick_vendor/COLCON_IGNORE \
-    ./src/tros/orocos_kinematics_dynamics/COLCON_IGNORE \
     ./src/tros/performance_test_fixture/COLCON_IGNORE \
-    ./src/tros/python_cmake_module/COLCON_IGNORE \
     ./src/tros/rcl/rcl/test/COLCON_IGNORE \
-    ./src/tros/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE \
-    ./src/tros/rcl_logging/rcl_logging_noop/COLCON_IGNORE \
-    ./src/tros/rcl_interfaces/test_msgs/COLCON_IGNORE \
-    ./src/tros/rclpy/COLCON_IGNORE \
     ./src/tros/realtime_support/COLCON_IGNORE \
     ./src/tros/rmw_connext/COLCON_IGNORE \
     ./src/tros/rmw_cyclonedds/COLCON_IGNORE \
-    ./src/tros/ros_testing/COLCON_IGNORE \
     ./src/tros/ros1_bridge/COLCON_IGNORE \
-    ./src/tros/ros2cli/COLCON_IGNORE \
-    ./src/tros/ros2cli_common_extensions/COLCON_IGNORE \
     ./src/tros/rosbag2/rosbag2_test_common/COLCON_IGNORE \
     ./src/tros/rosbag2/rosbag2_storage_evaluation/COLCON_IGNORE \
     ./src/tros/rosbag2/rosbag2_transport/COLCON_IGNORE \
     ./src/tros/rosbag2/rosbag2_tests/COLCON_IGNORE \
-    ./src/tros/rosidl_python/COLCON_IGNORE \
-    ./src/tros/rosidl_runtime_py/COLCON_IGNORE \
     ./src/tros/rosidl_typesupport_connext/COLCON_IGNORE \
-    ./src/tros/rpyutils/COLCON_IGNORE \
     ./src/tros/rviz/COLCON_IGNORE \
-    ./src/tros/sros2/COLCON_IGNORE \
-    ./src/tros/system_tests/COLCON_IGNORE \
-    ./src/tros/test_interface_files/COLCON_IGNORE \
     ./src/tros/tlsf/COLCON_IGNORE \
     ./src/tros/urdf/COLCON_IGNORE \
     ./src/tools/benchmark/COLCON_IGNORE \
