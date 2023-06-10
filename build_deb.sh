17ba3f9646d722e7694a3c6202602da49542add0
zixi01.chen <zixi01.chen@horizon.ai>
2023-06-10 11:12:59 +0800

    feat(mcap): [cr_id_skip] update mcap repo version

--- build_deb.sh
+++ build_deb.sh
@@ -96,7 +96,9 @@ function ros_base_colcon_ignore {
             ./src/box/hobot_sensors/COLCON_IGNORE \
             ./src/box/hobot_slam/COLCON_IGNORE \
             ./src/box/hobot_websocket/COLCON_IGNORE \
+            ./src/box/hobot_trigger/COLCON_IGNORE \
             ./src/tools/hobot_image_publisher/COLCON_IGNORE \
+            ./src/tools/hobot_visualization/COLCON_IGNORE \
             ./src/tools/benchmark/performance_test/COLCON_IGNORE
     elif [ "$platform" == "X86" ]; then
         touch \
@@ -122,7 +124,9 @@ function ros_base_colcon_ignore {
             ./src/box/hobot_sensors/COLCON_IGNORE \
             ./src/box/hobot_slam/COLCON_IGNORE \
             ./src/box/hobot_websocket/COLCON_IGNORE \
+            ./src/box/hobot_trigger/COLCON_IGNORE \
             ./src/tools/hobot_image_publisher/COLCON_IGNORE \
+            ./src/tools/hobot_visualization/COLCON_IGNORE \
             ./src/tools/benchmark/COLCON_IGNORE \
             ./src/box/ros-navigation/COLCON_IGNORE \
             ./src/tools/benchmark/performance_test/COLCON_IGNORE
