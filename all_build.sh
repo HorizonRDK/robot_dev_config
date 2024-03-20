a6d26948e0a562f2b94c19e222268e375a08abce
kao.zhu <kao.zhu@horizon.ai>
2024-03-20 22:34:08 +0800

    Update COLCON_IGNORE in all_build.sh

--- all_build.sh
+++ all_build.sh
@@ -7,6 +7,6 @@ touch \
     ./src/box/hobot_perception/hobot_stereonet/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_centerpoint/COLCON_IGNORE \
     ./src/box/hobot_slam/orb_slam3/COLCON_IGNORE \
-    ./src/tools/benchmark/performance_test/COLCON_IGNORE \
     ./src/tros/rosbag2/COLCON_IGNORE \
     ./src/box/hobot_trigger/COLCON_IGNORE \
+    ./src/box/hobot_llm/COLCON_IGNORE \
