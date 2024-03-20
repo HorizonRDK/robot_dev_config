5b0f220fe26d33979cf571eafa953385db3b98f0
kao.zhu <kao.zhu@horizon.ai>
2024-03-20 22:02:08 +0800

    Update COLCON_IGNORE in all_build.sh

--- all_build.sh
+++ all_build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 touch \
-    ./src/tros/performance_test_fixture/COLCON_IGNORE \
-    ./src/tros/rviz/COLCON_IGNORE \
+    ./src/tools/benchmark/performance_test/COLCON_IGNORE \
     ./src/tools/benchmark/performance_test_ros1_msgs/COLCON_IGNORE \
     ./src/tools/benchmark/performance_test_ros1_publisher/COLCON_IGNORE \
-    ./src/ros/ros_tutorials/COLCON_IGNORE \
-    ./src/tros/ros1_bridge/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_bev/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_stereonet/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_centerpoint/COLCON_IGNORE \
-    ./src/box/hobot_sensor/hobot_stereo_usb_cam/COLCON_IGNORE \
-    ./src/eProsima/compatibility/COLCON_IGNORE
+    ./src/box/hobot_slam/orb_slam3/COLCON_IGNORE \
+    ./src/tools/benchmark/performance_test/COLCON_IGNORE \
+    ./src/tros/rosbag2/COLCON_IGNORE \
+    ./src/box/hobot_trigger/COLCON_IGNORE \
